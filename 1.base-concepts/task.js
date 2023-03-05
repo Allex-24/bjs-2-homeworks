5153619a68d5524944a4e6fff0c0a4006606a9cb
Allex <inta.dn@yandex.ru>
2023-03-05 12:08:02 +0300

    Доработал первую задачу из первой лекции. Выполнил все задачи из второй лекции.

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,43 +1,43 @@
 "use strict"
 function solveEquation(a, b, c) {
 	let arr = [];
-	let dicriminant = b ** 2 - 4 * a * c;
+	let discriminant = b ** 2 - 4 * a * c;
 
-	if (dicriminant < 0) {
-		return arr;
-	} else if (dicriminant === 0) {
+	if (discriminant === 0) {
 		arr.push(-b / (2 * a));
-		return arr;
-	} else {
-		let root1 = (-b + Math.sqrt(dicriminant)) / (2 * a);
-		let root2 = (-b - Math.sqrt(dicriminant)) / (2 * a);
+	} else if (discriminant > 0) {
+		let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
 		arr.push(root1, root2);
-		return arr;
 	}
+
+	return arr;
+
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  // проверяем, что все аргументы являются числами
-  if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
-    return false;
-  }
+	// проверяем, что все аргументы являются числами
+	if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+		return false;
+	}
 
-  // преобразуем процентную ставку в месячную
-  let monthlyPercent = percent / 100 / 12;
+	// преобразуем процентную ставку в месячную
+	let monthlyPercent = percent / 100 / 12;
 
-  // вычисляем тело кредита
-  let loanBody = amount - contribution;
+	// вычисляем тело кредита
+	let loanBody = amount - contribution;
 
-  // вычисляем ежемесячный платеж
-  let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1)));
+	// вычисляем ежемесячный платеж
+	let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / (Math.pow(1 + monthlyPercent, countMonths) - 1)));
 
-  // вычисляем общую сумму, которую придётся заплатить клиенту
-  let totalAmount = monthlyPayment * countMonths;
+	// вычисляем общую сумму, которую придётся заплатить клиенту
+	let totalAmount = monthlyPayment * countMonths;
 
-  // округляем результат до двух значений после запятой
-  totalAmount = +totalAmount.toFixed(2);
+	// округляем результат до двух значений после запятой
+	totalAmount = +totalAmount.toFixed(2);
 
-  // возвращаем результат
-  return totalAmount;
+	// возвращаем результат
+	return totalAmount;
 }
 
